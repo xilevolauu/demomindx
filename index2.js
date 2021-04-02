1af1714fc9b6210ba8ab00ebf60cb017b9637c65
xilevolauu <74419072+xilevolauu@users.noreply.github.com>
2021-04-02 16:12:46 +0700

    a ngáo

--- index2.js
+++ index2.js
@@ -4,7 +4,7 @@ let pass = document.getElementById("pass");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (user.value == "admin" && pass.value == 123){
-        window.location.href="https://xilevolauu.github.io/demomindx/index2.js"
+        window.location.href="https://xilevolauu.github.io/demomindx/index2.html"
     }
     else{
         alert("Tên đăng nhập hoặc mật khẩu sai")
